fd739a044a6f19f0bcb3e19baa1b30f81cad00bf
Diarmuid Smith <smithdi@tcd.ie>
2022-04-07 15:00:08 +0100

    Implement additional functionality for dropPiece();

--- ConnectFour/src/Connect4Grid2DArray.java
+++ ConnectFour/src/Connect4Grid2DArray.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Field;
+
 public class Connect4Grid2DArray implements Connect4Grid
 {
     public static final int NUM_GRID_COLUMNS = 7;
@@ -27,6 +29,10 @@ public class Connect4Grid2DArray implements Connect4Grid
     @Override
     public boolean isValidColumn(int column)
     {
+        if (column >= 1 && column <= NUM_GRID_COLUMNS)
+        {
+            return true;
+        }
         return false;
     }
 
@@ -46,6 +52,24 @@ public class Connect4Grid2DArray implements Connect4Grid
     @Override
     public void dropPiece(ConnectPlayer player, int column)
     {
+        if (!isValidColumn(column))
+        {
+            System.out.println("That column is invalid. It must be in the range 1-7.");
+        }
+        else if (isColumnFull(column))
+        {
+            System.out.println("The column is full, please try again.");
+        }
+        else
+        {
+            for (int row = 0; row < NUM_GRID_ROWS; row++)
+            {
+                int currentIndexValue = grid[row][column];
+                if (row == NUM_GRID_ROWS - 1 && currentIndexValue == 0)
+                {
+                }
+            }
+        }
     }
 
     @Override
@@ -69,4 +93,6 @@ public class Connect4Grid2DArray implements Connect4Grid
         }
         return true;
     }
+
+
 }
